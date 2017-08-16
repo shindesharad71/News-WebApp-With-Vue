81cf7f0b646911c455a86ebaf6705320ccb07ada
Sharad Shinde <shindesharad71@gmail.com>
2017-08-17 03:03:51 +0530

    resource updated

--- src/components/Sources.vue
+++ src/components/Sources.vue
@@ -51,14 +51,54 @@ export default {
           img: 'http://www.dailymail.co.uk/apple-touch-icon.png'
         },
         {
-          title: 'ESPN',
-          link: 'espn',
-          img: 'http://a.espncdn.com/wireless/mw5/r1/images/bookmark-icons/espn_icon-152x152.min.png'
+          title: 'The TOI',
+          link: 'the-times-of-india',
+          img: 'https://icons.better-idea.org/lettericons/I-120-000000.png'
         },
         {
           title: 'Google News',
           link: 'google-news',
           img: 'https://ssl.gstatic.com/news-static/img/1703439073-news-thumb-128_w.png'
+        },
+        {
+          title: 'ESPN',
+          link: 'espn',
+          img: 'http://a.espncdn.com/wireless/mw5/r1/images/bookmark-icons/espn_icon-152x152.min.png'
+        },
+        {
+          title: 'Times',
+          link: 'times',
+          img: 'http://s0.wp.com/wp-content/themes/vip/time2014/img/time-touch_icon_120.png'
+        },
+        {
+          title: 'The Next Web',
+          link: 'the-next-web',
+          img: 'https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/apple-touch-icon-120x120.png?v=1502808859'
+        },
+        {
+          title: 'The New York Times',
+          link: 'the-new-york-times',
+          img: 'https://cdn1.nyt.com/mw-static/images/touch-icon-ipad-144.319373aa.png'
+        },
+        {
+          title: 'The Hindu',
+          link: 'the-hindu',
+          img: 'https://icons.better-idea.org/lettericons/T-120-ffffff.png'
+        },
+        {
+          title: 'Techradar',
+          link: 'techradar',
+          img: 'http://cdn0.static.techradar.futurecdn.net/20170811/apple-touch-icon.png'
+        },
+        {
+          title: 'Reddit All',
+          link: 'reddit-r-all',
+          img: 'https://www.redditstatic.com/mweb2x/favicon/120x120.png'
+        },
+        {
+          title: 'Hacker News',
+          link: 'hacker-news',
+          img: 'https://news.ycombinator.com/favicon.ico'
         }
       ]
     }
